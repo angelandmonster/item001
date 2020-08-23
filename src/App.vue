dc006b95ab84dc37eebaa91eb2b580d9b6304e29
吴冠 <605664467@qq.com>
2020-08-23 17:35:41 +0800

    m123

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="app">
+<div id="app">
     <!--路由占位符-->
     <router-view></router-view>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+    name: 'app',
 }
 </script>
 
